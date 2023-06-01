463dcf646e42f55f0a33a5507b72ec6d80c46a0a
FourtyThree43 <shaqmwa@outlook.com>
2023-06-02 00:57:02 +0300

    Task 4-9 + task files

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - Measures the height of a binary tree.
+ * @tree: A pointer to the root node of the tree to measure the height.
+ *
+ * Return: If tree is NULL, your function must return 0, else return height.
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height, right_height;
+
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+		return (0);
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+	return (left_height > right_height ? left_height + 1 : right_height + 1);
+}
--- 9-main.c
+++ 9-main.c
@@ -10,14 +10,20 @@
 int main(void)
 {
 	binary_tree_t *root;
+	size_t height;
 
 	root = binary_tree_node(NULL, 98);
 	root->left = binary_tree_node(root, 12);
 	root->right = binary_tree_node(root, 402);
-	binary_tree_print(root);
-	printf("\n");
 	binary_tree_insert_right(root->left, 54);
 	binary_tree_insert_right(root, 128);
 	binary_tree_print(root);
+
+	height = binary_tree_height(root);
+	printf("Height from %d: %lu\n", root->n, height);
+	height = binary_tree_height(root->right);
+	printf("Height from %d: %lu\n", root->right->n, height);
+	height = binary_tree_height(root->left->right);
+	printf("Height from %d: %lu\n", root->left->right->n, height);
 	return (0);
 }
